7e4dce0525d54e060f1ef43c3d39d7859bcc7390
Ashis <pradhanashish95@gmail.com>
2020-08-29 10:02:44 +0530

    Updates

---
+++ precache-manifest.70f6a61db54bd5d7fd846392ad20e04d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b31f0cb57c2a211f8f7ec5c396e9fa",
+    "url": "/DMV/index.html"
+  },
+  {
+    "revision": "f38997b11d15682004ae",
+    "url": "/DMV/static/css/main.8702420f.chunk.css"
+  },
+  {
+    "revision": "84e4c02b0f5e2958d41c",
+    "url": "/DMV/static/js/2.76f7fa2b.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/DMV/static/js/2.76f7fa2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f38997b11d15682004ae",
+    "url": "/DMV/static/js/main.a0ac541a.chunk.js"
+  },
+  {
+    "revision": "bb8b232f7bb7f6b14aaf",
+    "url": "/DMV/static/js/runtime-main.88310398.js"
+  }
+]);
